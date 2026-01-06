7966e4648f9c5d55702712052a72533b84281c69
Kunal Tanwar <kunal.t21@gmail.com>
2026-01-05 21:54:33 -0500

    Further work on tests

--- tests/database/test_db.py
+++ tests/database/test_db.py
@@ -2,13 +2,14 @@ import pytest
 import psycopg.errors
 
 # parametrize treats each set of inputs w/ the function as its own test, so reset_db will run between each input set
+# can add tests for ensuring email unique, and that created_at and updated_at are in fact timestamps
 @pytest.mark.asyncio
 @pytest.mark.parametrize(
     "email, password, should_raise_exc", 
     [
         ("bruh@email.com", "password123", False), # normal
-        ("", "password123", True), # null email (empty string)
-        ("bruh@email.com", "", True),
+        (None, "password123", True), # null email (empty string)
+        ("bruh@email.com", None, True),
           # null password
     ])
 async def test_insert_user(test_db, email, password, should_raise_exc):
@@ -18,6 +19,36 @@ async def test_insert_user(test_db, email, password, should_raise_exc):
             user = await test_db.insert_user(email, password)
     else:
         user = await test_db.insert_user(email, password)
-        # assert isinstance(user.id, int)
+        assert int(user.get("user_id")) is not None
         assert user.get("email") == email
         assert user.get("password") == password
+        assert user.get("created_at") is not None
+        assert user.get("updated_at") is not None
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize(
+    "email, password", 
+    [
+        ("bruh@email.com", "password123"),
+    ])
+async def test_get_user_by_email(test_db, email, password):
+    
+    await test_db.insert_user(email, password)
+    user = await test_db.get_user_by_email(email)
+
+    assert user.get("email") == email
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize(
+    "email, password", 
+    [
+        ("bruh@email.com", "password123"),
+    ])
+async def test_get_user_by_user_id(test_db, email, password):
+    
+    inserted_user = await test_db.insert_user(email, password)
+    user = await test_db.get_user_by_user_id(int(inserted_user.get("user_id")))
+    
+    assert user.get("email") == email
+
+# can run with "pytest -s" for print statements to show
